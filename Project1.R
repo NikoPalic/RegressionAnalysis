f8213740e56c88512c74b710d4da01ff022b6617
Niko Palić <niko.palic100@gmail.com>
2021-03-05 23:21:57 +0100

    Add files via upload

---
+++ Project1.R
@@ -0,0 +1,109 @@
+
+#install.packages("car") #needs libcurl installed
+library(car)
+
+df <- read.csv("bodyfatwomen.csv", header = TRUE)
+
+model <- lm(DEXfat ~ ., data=df)
+summary(model)
+
+#Residual analysis #############################################################
+#1) print residuals
+Rres = rstudent(model)
+res = rstandard(model)
+#Residuals 27 & 41 have high value -> outliers?
+#They're also higher as Rstudent vs Rstandard -> influential point?
+
+#2) plot the residuals
+plot(model, which=2)
+hist(res, breaks=30, main="RStandard residuals histogram")
+plot(res)
+text(res, labels=1:71, pos=4)
+#Very light tail on the right (positive skew), and a potential outlier at the very left
+
+#3) residual vs fitted value plot
+plot(model, which=1)
+#Kinda good, but potential problem at right end (higher fitted values)
+
+#4) residuals vs each regressor
+par(mfrow=c(2,3))
+plot(df$age, Rres, 
+       ylab="RStudent Residuals", xlab="Age", 
+       main="Full model: residuals vs regressor") 
+abline(0, 0)
+plot(df$waistcirc, Rres, 
+     ylab="RStudent Residuals", xlab="Waist circumference", 
+     main="Full model: residuals vs regressor") 
+abline(0, 0)
+plot(df$waistcirc, Rres, 
+     ylab="RStudent Residuals", xlab="Waist circumference", 
+     main="Full model: residuals vs regressor") 
+abline(0, 0)
+plot(df$hipcirc, Rres, 
+     ylab="RStudent Residuals", xlab="Hip circumference", 
+     main="Full model: residuals vs regressor") 
+abline(0, 0)
+plot(df$elbowbreadth, Rres, 
+     ylab="RStudent Residuals", xlab="Elbow breadth", 
+     main="Full model: residuals vs regressor") 
+abline(0, 0)
+plot(df$kneebreadth, Rres, 
+     ylab="RStudent Residuals", xlab="Knee breadth", 
+     main="Full model: residuals vs regressor") 
+abline(0, 0)
+#Some funneling going on? Especially for age -> uneven variance?
+par(mfrow=c(1,1))
+
+#5) Partial residual vs regressor plots 
+crp(model)
+#age, elbowbreadth, anthro3c uninformative (given others)?
+#kneebreadth is weird
+#rest are good
+
+#6) Cook's distance
+plot(model, which=4)
+influencePlot(model)
+#Residual 25 may be an influential (high leverage) outlier.
+#Residuals 27 & 41 & 48 may be outliers.
+
+#7) CovRatio
+covr = covratio(model)
+plot(covr, xlab="Residual Index")
+text(covr, labels=1:71, pos=4)
+#A few points seem to degrade precision
+
+#8) DFFITS
+dffits = dffits(model)
+plot(dffits)
+text(dffits, labels=1:71, pos=4)
+
+#Conclusion: remove 27, 41 and 48 
+dfr <- df[-c(27,41,48),]
+#Transformations ###############################################################
+
+modelr <- lm(DEXfat ~ ., data=dfr)
+summary(modelr)
+#R^2 increased from 0.9117 to 0.9434
+
+resr = rstandard(modelr)
+plot(resr) #seems to trend downwards?
+crp(modelr) #use crPlots(.., layout=c(1,1)) to choose one graph at a time (for the pdf)
+plot(modelr, which=2) #not very linear :(
+hist(resr, breaks=30)
+
+shapiro.test(resr) #it says it's normal
+
+#try to fix age:
+dft <- data.frame(dfr)
+dft$age <- sapply(dfr$age, function(x) x**3)
+modelt <- lm(DEXfat ~ ., data=dft)
+rest = rstandard(modelr)
+plot(rest) #A-OK
+crPlots(modelt)
+plot(modelt, which=2)
+
+#Conclusion: transformations upon the regressors don't fix the partial residual plots
+#The model behaves well without any transformations
+###############################################################################
+
+
